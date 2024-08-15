18c09086bfe8688bbabd7868b3a3ae534695722a
vignesh <vigneshcse005@gmail.com>
2024-08-15 20:43:55 +0530

    supplier form add

--- admin/footer.php
+++ admin/footer.php
@@ -1,3 +1,3 @@
 <div class="footer-wrap pd-20 mb-20 card-box">
-				HSE Admin By <a href="https://www.anandtechverce.com/" target="_blank">Anand Techverce</a>
+Sara Fabrication <a href="https://www.anandtechverce.com/" target="_blank">Link</a>
 			</div>
--- admin/include/config.php
+++ admin/include/config.php
@@ -1,21 +1,6 @@
 <?php
-/*
-if($_SERVER)
 
-{
-
-	$dbcon1 = mysqli_connect("localhost","lafsvapp_admin","Admin@54321","lafsvapp_admin") or die("localhost connection failed ");
-
-}
-
-else
-
-{
-*/
-	//$dbcon1 = mysqli_connect("localhost","hse_user","(JFBN5gXL~ED","hse_admin") or die("localhost connection failed ");
 	$dbcon1 = mysqli_connect("localhost","root","","hse_admin") or die("localhost connection failed ");
-
-//}
 
 
 
